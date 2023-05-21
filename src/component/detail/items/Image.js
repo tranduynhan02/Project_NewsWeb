350c4e93853b9db3be06d832b02843410ab291f6
TRANDANGQUOC <20130376@st.hcmuaf.edu.vn>
2023-05-21 14:04:39 +0700

    cap nhat trang chi tiet

--- src/component/detail/items/Image.js
+++ src/component/detail/items/Image.js
@@ -4,11 +4,11 @@ const Image = ({ link, p}) => {
     return (
         <div>
         <figure className="my-4">
-            <img  src={link} alt="" className="img-fluid" style={{maxWidth:"80%"}}/>
+            <img  src={link} alt="" className="img-fluid" style={{maxWidth:"90%"}}/>
             <figcaption>
             </figcaption>
         </figure>
-        <p style={{maxWidth:"800px"}}>{p}</p>
+        <p style={{maxWidth:"90%"}}>{p}</p>
         </div>
     );
 };
